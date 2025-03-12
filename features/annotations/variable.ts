072e37b08a3c6788f49328d8d8ef26c5c755f597
Sahil Shrestha <146646189+sahilstha0007@users.noreply.github.com>
2025-03-12 12:35:26 +0545

    object literals

---
+++ features/annotations/variable.ts
@@ -0,0 +1,34 @@
+let apples: number = 5;
+let speed: string = 'fast'
+let hasName: boolean = true;
+
+let notingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+// built in objects
+
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue']
+
+let myNumbers: number[] = [1, 2, 3];
+
+let truths: boolean[] = [true, true, false]
+
+
+// classes 
+class Car {
+  color: ""
+  height: ""
+}
+
+let car: Car = new Car();
+
+// Object literal 
+
+let point: { x: number, y: number } = {
+  x: 10,
+  y: 20
+}
